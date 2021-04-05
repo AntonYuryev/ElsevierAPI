95d6fc40164cb9a052d8e6abef2ec78d3dbe8159
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2021-04-05 09:24:15 -0400

    Update DatabaseStats.py

--- DatabaseStats.py
+++ DatabaseStats.py
@@ -2,8 +2,10 @@ import ElsevierAPI
 from ElsevierAPI import networx as PSnx
 import ElsevierAPI.ResnetAPI.ResnetAPISession as ssn
 import time
+import argparse
+import textwrap
 
-NON_DIRECTIONAL = ['Binding','FunctionalAssociation']
+NON_DIRECTIONAL = ['Binding','FunctionalAssociation','CellExpression','Metabolization']
 
 def IsDirectional(relType:str):
     return relType not in NON_DIRECTIONAL
@@ -20,26 +22,56 @@ def CountRelations (EntityIN, EntityOUT, RelType, isDirectional=True):
     return relcnt
 
 
-DBRelTypes = PSnx.GetRelationTypes()
-DBEntities = PSnx.GetEntityTypes()
-non_directional_counted = set()
 
-start_time = time.time()
-with open('DBstats.txt', 'w', encoding='utf-8') as f:
-    f.write('Regulator\tTarget\tRelation\tRelation counts\n')
-    for rel_type in DBRelTypes:
-        is_directional = IsDirectional(rel_type)
-        for EntityIN in DBEntities:
-            for EntityOUT in DBEntities:
-                if is_directional == False:
-                    if EntityOUT+EntityIN in non_directional_counted: 
-                        print('skipping: %s-%s-%s was already counted' % (EntityIN,rel_type,EntityOUT))
-                        continue
-                    else: non_directional_counted.update([EntityIN+EntityOUT])
+def GetStats(Triples:list):
+    non_directional_counted = set()
+    start_time = time.time()
+    with open('DBstats.txt', 'w', encoding='utf-8') as f:
+        f.write('Regulator\tTarget\tRelation\tRelation counts\n')
+        for triple in Triples:
+            EntityIN = triple[0]
+            rel_type = triple[1]
+            EntityOUT = triple[2]
 
-                rel_count = CountRelations(EntityIN,EntityOUT,rel_type,is_directional)
-                print('%s\t%s\t%s\t%d' % (EntityIN,EntityOUT,rel_type,rel_count))
-                if rel_count > 0:
-                    f.write('%s\t%s\t%s\t%d\n' % (EntityIN,EntityOUT,rel_type,rel_count))
+            is_directional = IsDirectional(rel_type)
+            if is_directional == False:
+                if EntityOUT+EntityIN in non_directional_counted: 
+                    print('skipping: %s-%s-%s was already counted' % (EntityIN,rel_type,EntityOUT))
+                    continue
+                else: non_directional_counted.update([EntityIN+EntityOUT])
 
-print("Time to retreive database stats: %s" % (ElsevierAPI.ExecutionTime(start_time)))
+            rel_count = CountRelations(EntityIN,EntityOUT,rel_type,is_directional)
+            print('%s\t%s\t%s\t%d' % (EntityIN,EntityOUT,rel_type,rel_count))
+            if rel_count > 0:
+                f.write('%s\t%s\t%s\t%d\n' % (EntityIN,EntityOUT,rel_type,rel_count))
+
+    print("Time to retreive database stats: %s" % (ElsevierAPI.ExecutionTime(start_time)))
+
+
+if __name__ == "__main__":
+    instructions = '''
+    infile - 3 column tab-delimted file with triplets description: Regulator<>Relation<tab>Target. 
+    If infile is not supplied counts for all possible triplets for all entity types and all relation types will be outputed
+    '''
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,epilog=textwrap.dedent(instructions))
+    parser.add_argument('-i', '--infile', default='')
+    args = parser.parse_args()
+
+    DBRelTypes = list()
+    DBEntities = list()
+    Triples = list()
+    if len(args.infile) == 0:
+        DBRelTypes = PSnx.GetRelationTypes()
+        DBEntities = PSnx.GetEntityTypes()
+        for rel_type in DBRelTypes:
+            for EntityIN in DBEntities:
+                for EntityOUT in DBEntities:
+                    Triples.append([EntityIN,rel_type,EntityOUT])
+    else:
+        with open(args.infile, 'r', encoding='utf-8') as f:
+            for line in f:
+                triple = line.strip().split('\t')
+                Triples.append(triple)
+
+        
+    GetStats(Triples)
