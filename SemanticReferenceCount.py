2fe36d8d0cc4846a770c7eefd8b16d36e109d2fa
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2021-05-12 21:46:33 -0400

    Update SemanticReferenceCount.py

--- SemanticReferenceCount.py
+++ SemanticReferenceCount.py
@@ -1,35 +1,35 @@
 import time
-import ElsevierAPI.ResnetAPI.ZeepToNetworkx as ZNx
+import ElsevierAPI.ResnetAPI.PathwayStudioGOQL as OQL
+from ElsevierAPI.ResnetAPI.ZeepToNetworkx import REF_ID_TYPES,PSNetworx
+from ElsevierAPI import networx as PSnx, ExecutionTime
 import pandas as pd
 import argparse
 import textwrap
-import ElsevierAPI
-import ElsevierAPI.ResnetAPI.PathwayStudioGOQL as OQL
-from ElsevierAPI import networx as PSnx
 
-
-ReferenceViewNewColumn1 = PSnx.mappedBy_propName
-ReferenceViewNewColumn2 = 'Concept name query'
-temp_id_col = 'entity_IDs'
-col_name_prefix = "RefCount to "
 
 class SearchParam:
     MappedEntityPandas = pd.DataFrame()
-    def __init__(self, EntityPandas:pd.DataFrame,prop_names_in_header,use_cache=False,need_references=False, AnnotateNodesWith=['Name']):
-        self.REL_PROPS=[]
-        self.ENT_PROPS=AnnotateNodesWith
+    def __init__(self, EntityPandas:pd.DataFrame,psnx:PSNetworx,prop_names_in_header,use_cache=False,need_references=False,AnnotateNodesWith=['Name']):
+        self.REL_PROPs=[]
+        self.ENT_PROPs=AnnotateNodesWith
         self.__cntCache='counts_cache.tsv'
         self.__refCache='reference_cache.tsv'
-        self.__mapfile='map_file.tsv'
+        self.api_session = psnx
+        self.__ReferenceViewNewColumn1 = self.api_session.mappedBy_propName
+        self.__ReferenceViewNewColumn2 = 'Concept name query'
+        self.__temp_id_col = 'entity_IDs'
+        self.__child_ids_propName = self.api_session.child_ids_propName
+        self.__col_name_prefix = "RefCount to "
         self.need_references=need_references
         if self.need_references == True:
-            self.REL_PROPS = ['Name','Sentence','PubYear','Title']
+            self.REL_PROPs = ['Name','Sentence','PubYear','Title']
 
         if use_cache == False:
             self.all_entity_ids = self.MapEntities(EntityPandas,prop_names_in_header)
         else:
             try: self.ReadCache()
             except FileNotFoundError:
+                print ('Cannot find cache file %s.  Will have to map entities on input identifiers again!' % self.__cntCache)
                 self.all_entity_ids = self.MapEntities(EntityPandas,prop_names_in_header)
 
 
@@ -47,7 +47,7 @@ class SearchParam:
         for propName in EntityPandas.columns:
             identifiers = list(RemainToMapPandas[propName])
             identifiers = list(map(str,identifiers))
-            propValToPSobj = PSnx.MapPropToEntities(identifiers,propName,set(self.ENT_PROPS),GetChilds=True)
+            propValToPSobj = PSnx.MapPropToEntities(identifiers,propName,set(self.ENT_PROPs),GetChilds=True)
             PropNameToPropToEntityID[propName] = propValToPSobj
             PropNameToPropToResnetName[propName] = propValToPSobj
             mapped_count = mapped_count + len(propValToPSobj)
@@ -66,7 +66,7 @@ class SearchParam:
                 obj_ids = [o['Id'] for o in mappedPSobj]
                 all_obj_ids = set().union(*obj_ids)
                 try:
-                    child_ids = [o[PSnx.child_ids_propName] for o in mappedPSobj]
+                    child_ids = [o[self.__child_ids_propName] for o in mappedPSobj]
                     all_obj_ids.update(set().union(*child_ids))
                     return resnet_names,str(cell_value),tuple(all_obj_ids)
                 except KeyError:
@@ -74,25 +74,25 @@ class SearchParam:
             except KeyError:
                 return None,None,None
         
-        def apply_and_concat(dataframe, field, func, column_names):
+        def apply_and_concat(dataframe:pd.DataFrame, field, func, column_names):
             return pd.concat((dataframe,dataframe[field].apply(lambda cell: pd.Series(func(field,cell),index=column_names))),axis=1)
 
         for propName in EntityPandas.columns:
             MappedEntitiesByProp = pd.DataFrame(EntityPandas)
-            MappedEntitiesByProp= apply_and_concat(MappedEntitiesByProp,propName,get_entIDs,['Resnet name',PSnx.mappedBy_propName,temp_id_col])
-            MappedEntitiesByProp = MappedEntitiesByProp[MappedEntitiesByProp[temp_id_col].notnull()]
+            MappedEntitiesByProp = apply_and_concat(MappedEntitiesByProp,propName,get_entIDs,['Resnet name',PSnx.mappedBy_propName,self.__temp_id_col])
+            MappedEntitiesByProp = MappedEntitiesByProp[MappedEntitiesByProp[self.__temp_id_col].notnull()]
             self.MappedEntityPandas = self.MappedEntityPandas.append(MappedEntitiesByProp)
 
-        print ('Mapped %d out of %d identifiers to entities in database in %s\n' % (len(self.MappedEntityPandas), len(EntityPandas.index),ElsevierAPI.ExecutionTime(start_mapping_time)))
-        entity_id_tuples = self.MappedEntityPandas[temp_id_col].tolist()
+        print ('Mapped %d out of %d identifiers to entities in database in %s\n' % (len(self.MappedEntityPandas),len(EntityPandas.index),ExecutionTime(start_mapping_time)))
+        entity_id_tuples = self.MappedEntityPandas[self.__temp_id_col].tolist()
         set_list = [set(x) for x in entity_id_tuples]
         return list(set.union(*set_list))
 
     def LinkToConcept(self,ConceptName,ConceptIDs:list,ConnectByRelTypes=[],RelEffect=[],RelDirection=''):
-        print('Linking input entities to %s concept with %d ontology childs' % (ConceptName,len(ConceptIDs)))
+        print('Linking input entities to \"%s\" concept with %d ontology childs' % (ConceptName,len(ConceptIDs)))
         if (len(ConceptIDs) > 500 and len(self.MappedEntityPandas) > 500):
             print('%s concept has %d ontology childs! Linking may take a while, be patient' % (ConceptName,len(ConceptIDs)-1))
-        NewColumnName = col_name_prefix+ ConceptName
+        NewColumnName = self.__col_name_prefix+ ConceptName
         self.MappedEntityPandas.insert(len(self.MappedEntityPandas.columns),NewColumnName,0)
 
         effecStr = ','.join(RelEffect)
@@ -103,7 +103,7 @@ class SearchParam:
 
         linked_entities_counter = 0
         start_time  = time.time()
-        FoundRelations = PSnx.SemanticRefCountByIds(ConceptIDs,self.all_entity_ids,ConnectByRelTypes,RelEffect,RelDirection,REL_PROPS=self.REL_PROPS, ENTITY_PROPS=self.ENT_PROPS)
+        FoundRelations = PSnx.SemanticRefCountByIds(ConceptIDs,self.all_entity_ids,ConnectByRelTypes,RelEffect,RelDirection,REL_PROPS=self.REL_PROPs, ENTITY_PROPS=self.ENT_PROPs)
         if FoundRelations.size() > 0:
             for regulatorID, targetID, rel in FoundRelations.edges.data('relation'):
                 try:
@@ -111,12 +111,12 @@ class SearchParam:
                 except KeyError:
                     entity_search_attr = ','.join(PSnx.Graph.nodes[targetID][PSnx.mappedBy_propName])
 
-                rel[ReferenceViewNewColumn1] = [entity_search_attr]
-                rel[ReferenceViewNewColumn2] = [ConceptName]
+                rel[self.__ReferenceViewNewColumn1] = [entity_search_attr]
+                rel[self.__ReferenceViewNewColumn2] = [ConceptName]
       
             AccumulateReference = set()
             for idx in self.MappedEntityPandas.index:
-                idx_entity_ids = list(self.MappedEntityPandas.at[idx,temp_id_col])
+                idx_entity_ids = list(self.MappedEntityPandas.at[idx,self.__temp_id_col])
                 reference_count = PSnx.CountReferences(idx_entity_ids,ConceptIDs,inGraph=FoundRelations)
                 self.MappedEntityPandas.at[idx,NewColumnName] = len(reference_count)
                 if len(reference_count) > 0:
@@ -125,18 +125,18 @@ class SearchParam:
             
             self.Print() #prints cache files for temp storage to handle network interruptions
             print("Concept \"%s\" is linked to %d rows from infile by %s relations of type \"%s\" supported by %d references with effect \"%s\" in %s" % 
-                (ConceptName,linked_entities_counter,FoundRelations.number_of_edges(),relTypeStr,len(AccumulateReference),effecStr,ElsevierAPI.ExecutionTime(start_time)))
+                (ConceptName,linked_entities_counter,FoundRelations.number_of_edges(),relTypeStr,len(AccumulateReference),effecStr,ExecutionTime(start_time)))
 
         else: print("Concept \"%s\" has no links to entities in infile" % (ConceptName))
         return linked_entities_counter
     
 
-    def Print (self, refCountsOut='', referencesOut=''):  
+    def Print (self, refCountsOut='', referencesOut='',psnx:PSNetworx=None):  
         PandasToPrint = pd.DataFrame(self.MappedEntityPandas)    
   
         if len(refCountsOut)>0: 
             countFile = refCountsOut
-            PandasToPrint = PandasToPrint.drop(temp_id_col,axis=1)
+            PandasToPrint = PandasToPrint.drop(self.__temp_id_col,axis=1)
             PandasToPrint = PandasToPrint.loc[(PandasToPrint!=0).any(axis=1)]
         else:
             print('Data is saved data to %s for protection or re-use' % self.__cntCache)
@@ -144,6 +144,7 @@ class SearchParam:
 
         PandasToPrint.to_csv(countFile, sep='\t', index=False)
 
+        if len(referencesOut) > 0: self.need_references = True
         if self.need_references==True:
             if len(referencesOut) > 0:
                 refFile = referencesOut
@@ -153,14 +154,14 @@ class SearchParam:
                 access_mode = 'a'
                 print('Supporting semantic triples are saved to %s for protection or re-use' % self.__refCache)
             
-            RelPropsToPrint = [ReferenceViewNewColumn1] + self.REL_PROPS + ZNx.REF_ID_TYPES + [ReferenceViewNewColumn2]
-            PSnx.PrintReferenceView(refFile,relPropNames=RelPropsToPrint,entPropNames=self.ENT_PROPS,access_mode=access_mode)
+            RelPropsToPrint = [self.__ReferenceViewNewColumn1] + self.REL_PROPs + self.api_session.REF_ID_TYPES + [self.__ReferenceViewNewColumn2]
+            self.api_session.PrintReferenceView(refFile,relPropNames=RelPropsToPrint,entPropNames=self.ENT_PROPs,access_mode=access_mode)
 
     def ReadCache(self):#returns last concept linked in cache
         try:
             self.MappedEntityPandas.to_csv(self.__cntCache,'\t')
             last_col_name = self.MappedEntityPandas.columns[-1]
-            return last_col_name[len(col_name_prefix):]
+            return last_col_name[len(self.__col_name_prefix):]
         except FileNotFoundError:
             print('Cache was not found! Will start processing all input files from scratch')
             return FileNotFoundError
@@ -264,7 +265,7 @@ if __name__ == "__main__":
             else:
                 search_param.LinkToConcept(PathwayName, pathway_components)
 
-        print("Entities in file %s were linked to %s pathway in %s" % (EntityListFile, PathwayName, ElsevierAPI.ExecutionTime(start_time)))
+        print("Entities in file %s were linked to %s pathway in %s" % (EntityListFile, PathwayName, ExecutionTime(start_time)))
     else:
         print('No pathways were specified for semantic linking with entities from \"%s\"' % (EntityListFile))
 
@@ -298,12 +299,12 @@ if __name__ == "__main__":
                     if pd.notna(dr): RelDirection = str(dr)
 
                 linked_entities_count= search_param.LinkToConcept(LinkToConcept,list(ConceptIDs),ConnectByRelTypes,RelEffect,RelDirection)
-                print("Total %d out of %d concepts interrogated in %s" % (conceptIdx+1, len(LinkToConcepts),ElsevierAPI.ExecutionTime(global_start_time)))
+                print("Total %d out of %d concepts interrogated in %s" % (conceptIdx+1, len(LinkToConcepts),ExecutionTime(global_start_time)))
 
-    print("%d entities in file %s were mapped linked to %d concepts from %s and %d concepts on cmd in %s" % (len(EntityPandas),EntityListFile,concepts_in_file,args.targets_file,concepts_in_cmd,ElsevierAPI.ExecutionTime(global_start_time)))
+    print("%d entities in file %s were mapped linked to %d concepts from %s and %d concepts on cmd in %s" % (len(EntityPandas),EntityListFile,concepts_in_file,args.targets_file,concepts_in_cmd,ExecutionTime(global_start_time)))
 
     countsOutFile = EntityListFile[:len(EntityListFile)-4]+'+SemanticRefcount.tsv'
     refOutFile = ''
     if search_param.need_references == True:
         refOutFile = EntityListFile[:len(EntityListFile)-4]+'+SemanticReferences.tsv'
-    search_param.Print(countsOutFile,refOutFile)
+    search_param.Print(countsOutFile,refOutFile,PSnx)
