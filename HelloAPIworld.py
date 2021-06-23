77ab841ae21b6432f1fbd7ad5c623e62934cd54b
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2021-06-22 20:17:13 -0400

    Add files via upload

---
+++ HelloAPIworld.py
@@ -0,0 +1,23 @@
+from ElsevierAPI.ResnetAPI.ResnetAPISession import APISession
+from ElsevierAPI import APIconfig
+from ElsevierAPI.ResnetAPI.NetworkxObjects import REF_PROPS,REF_ID_TYPES
+
+ps_api = APISession(APIconfig['ResnetURL'],APIconfig['PSuserName'],APIconfig['PSpassword'])
+# ps_api retreives data from the database and loads it into APISession.Graph derived from Networkx:MultiDiGraph 
+
+ps_api.add_rel_props(REF_PROPS+REF_ID_TYPES)
+#add_rel_props and add_end_props specify what attributes to retreive for relations and nodes (entities) from the database
+
+pcnt = '%'
+my_goql_query = 'select Relation where CellType LIKE '+pcnt+'hepatocyte'+pcnt+')'
+my_graph = ps_api.process_oql(my_goql_query)
+#process_oql retreives data by iterations. Iteration size is controled by ps_api.PageSize
+# which defaults to 100 relations per iteration
+# during retreival ps_api generates cache dump file with default name 'ResnetAPIsessionDump.tsv' using to_csv()
+# dump file delimeter is defined by ps_api.csv_delimeter
+# output file name is in ps_api.DumpFiles = ['ResnetAPIsessionDump.tsv']
+
+
+
+
+
