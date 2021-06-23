53f48a29d49cc4192f50973e33e0cf60ee240dbb
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2021-06-22 20:38:27 -0400

    Update __init__.py

--- ElsevierAPI/__init__.py
+++ ElsevierAPI/__init__.py
@@ -6,6 +6,3 @@ con_file = open("ElsevierAPI/APIconfig.json")  # file with your API keys and API
 APIconfig = json.load(con_file)
 con_file.close()
 
-# import ps_api session into your code to start retreival and processing. 
-# To create your own API session copy constructor below into your code
-ps_api = APISession(APIconfig['ResnetURL'], APIconfig['PSuserName'], APIconfig['PSpassword'])
