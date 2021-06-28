eb97291d0df7445a075f57ef8fec0155d6d6da75
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2021-06-28 11:20:41 -0400

    Add files via upload

--- TargetsForDrugsInClinicalTrials.py
+++ TargetsForDrugsInClinicalTrials.py
@@ -1,19 +1,40 @@
 from ElsevierAPI.ResnetAPI.ResnetGraph import ResnetGraph
 import time
 from ElsevierAPI import APIconfig
+import ElsevierAPI.ResnetAPI.PathwayStudioGOQL as OQL
 from ElsevierAPI.ResnetAPI.ResnetAPISession import APISession
 from ElsevierAPI.ResnetAPI.NetworkxObjects import REF_PROPS,REF_ID_TYPES
 import networkx as nx
 import xml.etree.ElementTree as et
+from xml.dom import minidom
 
-search_antagonists = True
-start = time.time()
+input_drug_list = 'ListOfDrugs.txt'
 ps_api = APISession(APIconfig['ResnetURL'],APIconfig['PSuserName'],APIconfig['PSpassword'])
 ps_api.DumpFiles.clear()
 ps_api.add_rel_props(REF_PROPS+REF_ID_TYPES+['Effect'])
 ps_api.PageSize = 1000
-oql_query = 'SELECT Relation WHERE objectType = ClinicalTrial AND TrialStatus = Completed AND NeighborOf (SELECT Entity WHERE objectType = SmallMol)'
-completed_trials = ps_api.process_oql(oql_query, debug=False)
+working_dir = 'D:/Python/CTs/'
+
+def retreive_clinical_trials(drugs_name_file=None):
+    request_name = 'Find all clinical trials'
+    oql_query = 'SELECT Relation WHERE objectType = ClinicalTrial AND TrialStatus = Completed AND NeighborOf (SELECT Entity WHERE objectType = SmallMol)'
+    if isinstance(drugs_name_file, str):      
+        with open(drugs_name_file) as f:
+            drug_names = [line.rstrip('\n') for line in f]
+        print('Read %s with %d drug names' %(drugs_name_file,len(drug_names)))
+
+        search_by_prop, drug_names_str = OQL.get_search_strings(['Name','Alias'],drug_names)
+        oql_query =  oql_query + ' AND NeighborOf (SELECT Entity WHERE (Name,Alias)= ({names}))'
+        oql_query = oql_query.format(names = drug_names_str)
+        request_name = 'Find clinical trials for drugs in {fname}'
+        request_name = request_name.format(fname=drugs_name_file)
+
+    return ps_api.process_oql(oql_query,request_name, debug=False)
+
+search_antagonists = True
+start = time.time()
+my_drug_list = working_dir + input_drug_list
+completed_trials = retreive_clinical_trials(my_drug_list)
 drug_ids = set(completed_trials.get_entity_ids(['SmallMol']))
 print('Found %d clinical trials with %d drugs' % (completed_trials.number_of_edges(), len(drug_ids)))
 
@@ -25,43 +46,48 @@ drug_with_targets = set(drug_targets.get_entity_ids(['SmallMol']))
 print('Found %d drugs with targets and clinical trials' % len(drug_with_targets))
 
 drug_counter = 0
-from xml.dom import minidom
-fout_name = 'drug_targets_for_indications'
-with open(fout_name+'.rnef', mode='w', encoding='utf-8') as rnefout:
-    rnefout.write('<?xml version="1.0" ?>\n<batch>\n')
-    start = time.time()
-    for drug in drug_with_targets:
-        drug_counter += 1
-        target_ids = set(drug_targets.get_neighbors({drug}))
-        indication_ids = completed_trials.get_neighbors({drug})
+out_dir = working_dir+'DrugTargets/'
+output_all_targets = False
+start = time.time()
+for drug in drug_with_targets:
+    drug_counter += 1
+    target_ids = set(drug_targets.get_neighbors({drug}))
+    indication_ids = completed_trials.get_neighbors({drug})
 
-        effect = 'positive' if search_antagonists else 'negative'
-        targets_indications = ps_api.connect_nodes(target_ids,indication_ids,with_effect=[effect])
-        if targets_indications.size() == 0: continue
+    effect = 'positive' if search_antagonists else 'negative'
+    targets_indications = ps_api.connect_nodes(target_ids,indication_ids,with_effect=[effect])
+    if targets_indications.size() == 0: continue
 
-        drug_target_indication = ResnetGraph()
-        drug_target_indication.add_graph(completed_trials.get_neighbors_graph({drug}))
-        drug_target_indication.add_graph(drug_targets.get_neighbors_graph({drug}))
-        drug_target_indication.add_graph(targets_indications)
-        drug_target_indication.count_references()
+    drug_target_indication = ResnetGraph()
+    drug_target_indication.add_graph(completed_trials.get_neighbors_graph({drug}))
+    drug_target_indication.add_graph(drug_targets.get_neighbors_graph({drug}))
+    drug_target_indication.add_graph(targets_indications)
+    drug_target_indication.count_references()
+    if not output_all_targets:
+        targets_without_indications = set([x for x, y in drug_target_indication.nodes(data=True) if
+                        ((drug_target_indication.degree(x) == 1) & 
+                        (y['ObjTypeName'][0] in ['Protein','Complex','FunctionalClass']))])
+        drug_target_indication.remove_nodes_from(targets_without_indications)
 
-        drug_name = drug_target_indication.get_properties({drug},'Name')[drug][0]
-        pathway_name = drug_name +' targets in clinical trials'
-        rnef_xml = et.fromstring(ps_api.to_rnef(drug_target_indication))
-        rnef_xml.set('name', pathway_name)
-        rnef_xml.set('type','Pathway')
-        
-        TargetCount = set([x for x, y in drug_target_indication.nodes(data=True) if
-                            ((drug_target_indication.degree(x) > 1) & (y['ObjTypeName'][0] in ['Protein','Complex','FunctionalClass']))])
+    drug_name = drug_target_indication.get_properties({drug},'Name')[drug][0]
+    pathway_name = drug_name +' targets in clinical trials'
+    pathway_rnef = et.fromstring(ps_api.to_rnef(drug_target_indication))
+    pathway_rnef.set('name', pathway_name)
+    pathway_rnef.set('type','Pathway')
+    batch_xml = et.Element('batch')
+    batch_xml.insert(0,pathway_rnef)
+    
+    TargetCount = set([x for x, y in drug_target_indication.nodes(data=True) if
+                        ((drug_target_indication.degree(x) > 1) & 
+                        (y['ObjTypeName'][0] in ['Protein','Complex','FunctionalClass']))])
 
-        print('Found %d targets for %s in %d indications. %d in %d drugs processed in %s' % 
-            (len(TargetCount),drug_name,len(indication_ids),drug_counter, len(drug_with_targets),ps_api.execution_time(start)))   
-        #batch_xml.append(rnef_xml)
-        pathway_rnef = et.tostring(rnef_xml,encoding='utf-8').decode("utf-8")
-        pathway_rnef = minidom.parseString(pathway_rnef).toprettyxml(indent='   ')
-        rnefout.write(pathway_rnef)
-        ps_api.Graph.clear() # need to release memory when performing large dumps
+    print('Found %d targets for %s in %d indications. %d in %d drugs processed in %s' % 
+        (len(TargetCount),drug_name,len(indication_ids),drug_counter, len(drug_with_targets),ps_api.execution_time(start)))   
+    pathway_rnef_str = et.tostring(batch_xml,encoding='utf-8',xml_declaration=True).decode("utf-8")
+    pathway_rnef_str = minidom.parseString(pathway_rnef_str).toprettyxml(indent='   ')
+    with open(out_dir+pathway_name+'.rnef', mode='w', encoding='utf-8') as rnefout:
+        rnefout.write(pathway_rnef_str)
 
-    rnefout.write('</batch>')
+    ps_api.Graph.clear() # need to release memory when performing large dumps
 
-print('Your data was downloaded in %s into %s file' % (ps_api.execution_time(start),fout_name))
+print('Pathways with drug targets are in %s into %s directory' % (ps_api.execution_time(start),out_dir))
