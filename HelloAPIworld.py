14a63736c74f8d001b9565df735c44023c23217f
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2021-07-07 12:57:08 -0400

    Update HelloAPIworld.py

--- HelloAPIworld.py
+++ HelloAPIworld.py
@@ -11,10 +11,13 @@ ps_api.add_rel_props(REF_PROPS+REF_ID_TYPES)
 pcnt = '%'
 my_goql_query = 'select Relation where CellType LIKE \''+pcnt+'hepatocyte'+pcnt+'\''
 my_goql_query = 'select Relation where objectType=Expression AND CellType LIKE \'' + pcnt + 'hepatocyte' + pcnt + '\''
+my_goql_query = 'select Relation where objectType=StateChange AND CellType LIKE \'' + pcnt + 'hepatocyte' + pcnt + '\''
 request_name = 'Find relations reported in hepatocytes'
-my_graph = ps_api.process_oql(my_goql_query,request_name, debug=True, flush_dump=True)
+my_graph = ps_api.process_oql(my_goql_query,request_name, debug=False, flush_dump=True)
 # process_oql retreives data by iterations. Iteration size is controled by ps_api.PageSize
 # ps_api.PageSize defaults to 100 relations per iteration and cannot be bigger than 10,000
+# debug=False retreives resuts only from the first iteration
+# #set debug=True to retreive all results from your GOQL query. 
 # during retreival ps_api caches data into dump file with default name 'ResnetAPIsessionDump.tsv' using APISession.to_csv()
 # dump file delimeter is defined by APISession.csv_delimeter parameter
 # dump file name is in ps_api.DumpFiles = ['ResnetAPIsessionDump.tsv']
