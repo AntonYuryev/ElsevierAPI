42076f8d401ccd136b48b1fffbeacfd7f0ddbf33
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2021-09-13 11:21:42 -0400

    Add files via upload

--- ElsevierAPI/__init__.py
+++ ElsevierAPI/__init__.py
@@ -1,8 +1,23 @@
-from ElsevierAPI.ResnetAPI.ResnetAPISession import APISession
 import json
+from ElsevierAPI.ResnetAPI.ResnetAPISession import APISession
 
+config_dir = 'D:/Python/ENTELLECT_API/ElsevierAPI/'
+apiconfig = 'APIconfig.json'
+default_apiconfig = config_dir + apiconfig
 
-con_file = open("ElsevierAPI/APIconfig.json")  # file with your API keys and API URLs
-APIconfig = json.load(con_file)
-con_file.close()
+def load_api_config(api_config_file=None):# file with your API keys and API URLs
+    if not isinstance(api_config_file,str):
+        print('No API config file was specified\nWil use default %s instead'% default_apiconfig)
+        api_config_file = default_apiconfig
+    try:
+        return json.load(open(api_config_file))
+    except FileNotFoundError:
+        print("Cannot find API config file: %s" % api_config_file)
+        if api_config_file != default_apiconfig:
+            return json.load(open(default_apiconfig))
+        else:
+            print('No working API server was specified!!! Goodbye')
 
+def open_api_session(api_config_file=None) -> APISession:
+    APIconfig = load_api_config(api_config_file)
+    return APISession(APIconfig['ResnetURL'], APIconfig['PSuserName'], APIconfig['PSpassword'])
