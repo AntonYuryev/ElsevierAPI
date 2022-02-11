255d11e8d9760acaaa3649f22accd4d3f96bf782
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2022-02-11 07:26:52 -0500

    Add files via upload

--- ElsevierAPI/__init__.py
+++ ElsevierAPI/__init__.py
@@ -1,24 +1,31 @@
 import json
 from ElsevierAPI.ResnetAPI.ResnetAPISession import APISession
+from datetime import timedelta
+import time
 
-#config_dir = 'D:/Python/ENTELLECT_API/ElsevierAPI/'
 config_dir = './ElsevierAPI/'
 apiconfig = 'APIconfig.json'
 default_apiconfig = config_dir + apiconfig
 
 def load_api_config(api_config_file=None):# file with your API keys and API URLs
     if not isinstance(api_config_file,str):
-        print('No API config file was specified\nWil use default %s instead'% default_apiconfig)
+        print('No API config file was specified\nWill use default %s instead'% default_apiconfig)
         api_config_file = default_apiconfig
     try:
-        return json.load(open(api_config_file))
+        return dict(json.load(open(api_config_file)))
     except FileNotFoundError:
         print("Cannot find API config file: %s" % api_config_file)
         if api_config_file != default_apiconfig:
-            return json.load(open(default_apiconfig))
+            print('Cannot open %s config file\nWill use default %s instead'% (api_config_file, default_apiconfig))
+            return dict(json.load(open(default_apiconfig)))
         else:
             print('No working API server was specified!!! Goodbye')
+            return None
 
 def open_api_session(api_config_file=None) -> APISession:
     APIconfig = load_api_config(api_config_file)
     return APISession(APIconfig['ResnetURL'], APIconfig['PSuserName'], APIconfig['PSpassword'])
+
+
+def execution_time(execution_start):
+    return "{}".format(str(timedelta(seconds=time.time() - execution_start)))
