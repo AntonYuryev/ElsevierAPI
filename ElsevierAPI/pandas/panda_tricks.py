93355d7c89520e2be859dad50e49cd604954b8ad
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2022-06-01 14:15:43 -0400

    Add files via upload

--- ElsevierAPI/pandas/panda_tricks.py
+++ ElsevierAPI/pandas/panda_tricks.py
@@ -3,10 +3,13 @@ from pandas import DataFrame as df
 import rdfpandas
 from ..ResnetAPI.ResnetGraph import PSObject
 
+
 class df(pd.DataFrame):
     pass
-    def __init__(self, p=pd.DataFrame()):
-        super().__init__(p)
+    def __init__(self, p=pd.DataFrame(),**kwargs):
+        if kwargs:
+            p = pd.DataFrame(**kwargs)
+        super(df,self).__init__(p)
 
     def format_vertical_headers(self):
         """Display a dataframe with vertical column headers"""
@@ -87,10 +90,10 @@ class df(pd.DataFrame):
             return in2pd.merge(obj_pd,how, on=map2column)
 
 
-    def get_row(self, by_value1, in_column1, and_by_value2, in_column2):
+    def get_rows(self, by_value1, in_column1, and_by_value2, in_column2):
         return self.loc[(self[in_column1] == by_value1) & (self[in_column2] == and_by_value2)]
 
-    def get_cell(self,by_value1, in_column1, and_by_value2, in_column2, from_column3):
+    def get_cells(self,by_value1, in_column1, and_by_value2, in_column2, from_column3):
         return (self.loc[(self[in_column1] == by_value1) & (self[in_column2] == and_by_value2),from_column3]).iloc[0]
 
     def df2json(self, to_file:str, dir=''):
@@ -100,4 +103,33 @@ class df(pd.DataFrame):
         with open(dump_fname, 'w') as jsonout:
             self.reset_index(inplace=True)
             jsonout.write(self.to_json(None,indent=2, orient='index'))
+
+
+    def df2excel(self, writer:pd.ExcelWriter,sheet_name:str, index=False, vertical_header=False):
+        self.to_excel(writer, sheet_name=sheet_name, startrow=1, header=False, index=index)
+        workbook  = writer.book
+        worksheet = writer.sheets[sheet_name]
+
+        header_format = {
+                    'bold': True,
+                    'text_wrap': True,
+                    'valign': 'vjustify',#'bottom',#'top', #vcenter',
+                    'align': 'center' # 'left' 
+                    #'fg_color': '#D7E4BC',
+                    #'border': 1,
+                    }
+
+        header_height = 15
+        if vertical_header:
+            header_format.update({'rotation': '90'})
+            header_height = 120
+
+        format = workbook.add_format(header_format)
+        worksheet.set_row(0,header_height,format)
+        for col_num, value in enumerate(self.columns.values):
+            worksheet.write(0, col_num, value,format)
+
+
+        
+
 
