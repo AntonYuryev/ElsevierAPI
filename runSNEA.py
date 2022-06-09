1c8c4b4a8466aa468e60c920b87f0dcbbebbb3e5
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2022-06-09 11:43:06 -0400

    cosmetic

--- runSNEA.py
+++ runSNEA.py
@@ -2,10 +2,8 @@
 from ElsevierAPI import load_api_config
 from ElsevierAPI.ResnetAPI.SNEA import SNEA
 
-#exp = Experiment('PNOC003 transcription regulators activity',load_api_config() )
-APIcofig = 'D:/Python/ENTELLECT_API/ElsevierAPI/APIconfigTeva.json'
-#APIcofig = ''
-snea = SNEA(load_api_config(APIcofig),'Drug pipeline testing') #'PNOC003vsGSE120046') #
+APIcofig = ''
+snea = SNEA(load_api_config(APIcofig),'Drug pipeline testing')
 snea.expression_regulators()
 snea.report()
 #snea.test_run(fast=False)
