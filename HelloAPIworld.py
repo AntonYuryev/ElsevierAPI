dc244b4b52731881ba3d90fb920f081960fc0aaa
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2022-07-07 23:23:54 -0400

    Add files via upload

--- HelloAPIworld.py
+++ HelloAPIworld.py
@@ -2,7 +2,7 @@ from ElsevierAPI import open_api_session
 from ElsevierAPI.ETM_API.references import PS_ID_TYPES,PS_BIBLIO_PROPS,SENTENCE_PROPS,CLINTRIAL_PROPS,RELATION_PROPS
 
 # ps_api retreives data from the database and loads it into APISession.Graph derived from Networkx:MultiDiGraph 
-ps_api = open_api_session(api_config_file=None)#specify here path to your APIconfig file. 
+ps_api = open_api_session(api_config_file='')#specify here path to your APIconfig file. 
 #If api_config_file not specified the default APIConfig from __init__.py will be used
 
 all_relation_properties = list(PS_ID_TYPES)+list(PS_BIBLIO_PROPS)+list(SENTENCE_PROPS)+list(CLINTRIAL_PROPS)+list(RELATION_PROPS)
@@ -27,7 +27,12 @@ if __name__ == "__main__":
     ps_api.start_download_from(0) #if download was interrupted change this paramater to resume download from certain position
 #position must be specified as the number of relations (or entities) downloaded previously
 
+    my_goql_query = 'SELECT Relation WHERE NeighborOf (SELECT Entity WHERE Name = PKC) AND NeighborOf (SELECT Entity WHERE Name = PDPK1)'
     my_graph = ps_api.process_oql(my_goql_query,request_name, debug=False, flush_dump=True)
+
+    ps_api.csv_delimeter = '\t'
+    ps_api.to_csv('my_graph.tsv')
+    ps_api.graph2rnef('my_graph.rnef')
 # process_oql retreives data by iterations. Iteration size is controled by ps_api.PageSize
 # ps_api.PageSize defaults to 100 relations per iteration and cannot be bigger than 10,000
 # debug=False retreives resuts only from the first iteration
