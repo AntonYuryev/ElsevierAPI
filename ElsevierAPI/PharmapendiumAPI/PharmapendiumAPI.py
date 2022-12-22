5eb52bed51769c68cb0c7924bd30164043616e81
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2022-12-22 12:28:41 -0500

    Add files via upload

--- ElsevierAPI/PharmapendiumAPI/PharmapendiumAPI.py
+++ ElsevierAPI/PharmapendiumAPI/PharmapendiumAPI.py
@@ -19,19 +19,41 @@ class Pharmapendium:
     def _url_request(self):
         return self.url+self._get_param_str()
     
-    def _get_results(self):
+    
+    def __get_results(self):
         try:
             req = urllib.request.Request(url=self._url_request(), headers=self.headers)
             response = urllib.request.urlopen(req)
         except http_error.HTTPError:
             sleep(30)
+            try:
+                req = urllib.request.Request(url=self._url_request(), headers=self.headers)
+                response = urllib.request.urlopen(req)
+            except http_error.HTTPError:
+                raise http_error.HTTPError
+
         pp_view = response.read()
         result = json.loads(pp_view.decode('utf-8'))
         sleep(0.34)
         return result
 
+
     def _add_param(self,to_add:dict()):
         self.params.update(to_add)
+
+
+    def get_results(self):
+        result = self.__get_results()
+        result_count = result['data']['countTotal']
+
+        all_items = list()
+        for page in range(0,result_count,self.page_size):
+            self._add_param({'limitation.firstRow':page+self.page_size})
+            result = self.__get_results()
+            items = result['data']['items']
+            all_items += items
+
+        return all_items
 
 
 class SafetyPP(Pharmapendium):
@@ -42,11 +64,12 @@ class SafetyPP(Pharmapendium):
     def GetTopEffectCategory(self,taxName):
         add_params = {'taxonomy': 'Effects', 'query':taxName}
         self._add_param(add_params)
-        result = self._get_results()
+        result = self.__get_results()
         if result > 0:
             return result['children'][0]['data']['name']
         else: 
             return ''
+
 
 class DrugIndications(Pharmapendium):
     def __init__(self,search_type:str, APIconfig:dict):
@@ -55,7 +78,7 @@ class DrugIndications(Pharmapendium):
 
     def drugs4indication(self,indications:list):
         self._add_param({'indications':indications})
-        result = self._get_results()
+        result = self.__get_results()
         if result:
             return result['data']['items']
         else: 
@@ -69,7 +92,7 @@ class DrugActivity(Pharmapendium):
 
     def targets4(self,drugs:list):
         self._add_param({'drugs':drugs})
-        result = self._get_results()
+        result = self.__get_results()
         if result:
             return result['data']['items']
         else: 
