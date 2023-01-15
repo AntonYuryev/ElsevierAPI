3ebeae5895b375944206ecb1e7a527d9d3ad0554
Anton Yuryev <66886638+AntonYuryev@users.noreply.github.com>
2023-01-15 16:37:44 -0500

    Add files via upload

--- DaaS.py
+++ DaaS.py
@@ -2,6 +2,8 @@ from ElsevierAPI import open_api_session
 from ElsevierAPI.ResnetAPI.ResnetAPISession import SNIPPET_PROPERTIES,BIBLIO_PROPERTIES,REFERENCE_IDENTIFIERS,DATABASE_REFCOUNT_ONLY
 from ElsevierAPI.ResnetAPI.ResnetGraph import REFCOUNT,EFFECT
 from ElsevierAPI.ResnetAPI.PathwayStudioGOQL import OQL
+from ElsevierAPI.ETM_API.references import pubmed_hyperlink, make_hyperlink
+from ElsevierAPI.pandas.panda_tricks import ExcelWriter,pd
 
 ps_api = open_api_session(api_config_file='',what2retrieve=REFERENCE_IDENTIFIERS)
 ps_api.add_rel_props([EFFECT])
@@ -18,4 +20,16 @@ if __name__ == "__main__":
     fname = request_name+' relations' if ps_api.print_rel21row else request_name+' references'
     ps_api.entProps = ['ObjTypeName','Name']
     ps_api.relProps = ['ObjTypeName','Effect',REFCOUNT,'PMID','DOI']
-    ps_api.to_csv(fname+'.tsv')
+    daas_table = ps_api.to_pandas()
+    clickable_refcount = 'Number of reference. Link opens publications in PubMed'
+    #daas_table[clickable_refcount] = pubmed_hyperlink(daas_table['PMID'].split(','),daas_table[REFCOUNT])
+    for idx in daas_table.index:
+        pmids = daas_table.at[idx,'PMID']
+        pmid_list = [] if pd.isna(pmids) else pmids.split(',')
+        daas_table.at[idx,clickable_refcount] = pubmed_hyperlink(pmid_list, daas_table.at[idx,REFCOUNT])
+    daas_table.drop(columns=[REFCOUNT],inplace=True)
+    daas_table.set_hyperlink_color([clickable_refcount])
+    writer = ExcelWriter(fname+'.xlsx', engine='xlsxwriter')
+    daas_table.df2excel(writer,request_name[:30])
+    writer.save()
+
